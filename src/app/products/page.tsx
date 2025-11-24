7a6db6a0ba2a9138ac44027dcd6d6d9d62a0ba2b
Temp445 <acetemp345@gmail.com>
2025-11-24 18:38:36 +0530

    solve the netlify error

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,7 +1,13 @@
-import ProductsClient from "./ProductsClient";
+import { Suspense } from "react";
+import ProductsClient from './ProductsClient'
+export const dynamic = "force-dynamic";
 
 const ProductPage = () => {
-  return <ProductsClient />;
-};
+  return (
+    <Suspense fallback={<div className="p-8 text-center"></div>}>
+        <ProductsClient />
+    </Suspense>
+  )
+}
 
-export default ProductPage;
+export default ProductPage
